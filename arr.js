c870610dc96b2a063b224f80ed1c0ec9b52d1939
ShuwenHe <1201220707@pku.edu.cn>
2025-02-23 14:36:42 +0800

    feat:timestamp

---
+++ arr.js
@@ -0,0 +1,7 @@
+let a = [];
+a.push(1,5,3,9,7);
+a.reverse();
+console.log(a);
+a.sort((a,b) => a - b);
+console.log(a);
+
---
+++ plus.js
@@ -0,0 +1,7 @@
+let x,y = 3;
+
+function plus(x){
+	return x + 1;
+}
+
+plus(y)
